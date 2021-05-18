e0d8c87a9adb66da36a721c8b37b444029acaa9c
Erin Woodrum <erin.cook.dev@gmail.com>
2021-05-18 15:43:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+size_of_board = 600
+number_of_dots = 6
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 50
+dot_color = '#7BC043'
+player1_color = '#0492CF'
+player1_color_light = '#67B0CF'
+player2_color = '#EE4035'
+player2_color_light = '#EE7E77'
+Green_color = '#7BC043'
+distance_between_dots = size_of_board / number_of_dots 
+dot_width = .25 * distance_between_dots
+edge_width = .1 * distance_between_dots
+
+
